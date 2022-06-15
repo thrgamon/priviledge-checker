2615d13f5a4e41869a9eb581d6df670ded0648d2
Tom Gamon <tom@tomgamon.com>
2022-06-15 17:51:28 +1000

    Update chart js

--- Check your privilege!_files/myChart.js
+++ Check your privilege!_files/myChart.js
@@ -1,42 +1,34 @@
-//Data Variables
-var SkinColour = 0;
-var Education = 0;
-var Ability = 0;
-var Sexuality = 0;
-var Neurodiversity = 0;
-var MentalHealth = 0;
-var Body = 0;
-var Housing = 0;
-var Wealth = 0;
-var Language = 0;
-var Gender = 0;
-var Citizenship = 0;
-
-
 //Labels
-const Labels = [
-"Skin Colour", 
-"Formal Education", 
-"Ability", 
-"Sexuality", 
-"Neurodiversity", 
-"Mental Health", 
-"Body size", 
-"Housing", 
-"Wealth", 
-"Language", 
-"Gender", 
-"Citizenship",
-];
-
+// Here we store the labels as an object, where we can store key/value pairs.
+// This means we can correspond a particular label, with a particular value,
+// without having to track both seperately.
+// You will notice I relabelled the inputs in the html to match the labels here.
+// You will see why in a second.
+const Labels = {
+"Skin Colour": 0,
+"Formal Education": 0,
+"Ability": 0,
+"Sexuality": 0,
+"Neurodiversity": 0,
+"Mental Health": 0,
+"Body Size": 0,
+"Housing": 0,
+"Wealth": 0,
+"Language": 0,
+"Gender": 0,
+"Citizenship": 0,
+};
 
 
 //Radar chart data
 var radarData = {
-  labels: Labels,
+  // Now our labels is an object, we can use this Object.keys() function
+  // to grab all the keys, aka all the labels.
+  labels: Object.keys(Labels),
   datasets: [{
-    data:  [SkinColour, Education, Ability, Sexuality, Neurodiversity, MentalHealth, Body, Housing, Wealth, Language, Gender, Citizenship],
-	label: '', 
+    // Similarly we can grab all the values
+    data: Object.values(Labels),
+	label: '',
    	fill: false,
     backgroundColor: 'rgba(255, 0, 0, 0.2)',
     borderColor: 'rgb(255, 0, 0)',
@@ -63,7 +55,7 @@ const radarOptions = {
                 display: false,
             }
         }
-    
+
 };
 
 
@@ -78,102 +70,31 @@ const config = {
 var chart = new Chart(document.getElementById("radarChart"), config);
 
 
-
-
-
-//This works for to update each individual variable to the new radio value and then updates the chart
 function updateForm() {
-    var radios = document.getElementsByName('SkinColour');
-    for (var radio of radios)
-    {
-        if (radio.checked) {
-            SkinColour = radio.value;
-        }
-    }
-    var radios = document.getElementsByName('Education');
-    for (var radio of radios)
-    {
-        if (radio.checked) {
-            Education = radio.value;
-        }
-    }
-    var radios = document.getElementsByName('Ability');
-    for (var radio of radios)
-    {
-        if (radio.checked) {
-            Ability = radio.value;
-        }
-    }
-    var radios = document.getElementsByName('Sexuality');
-    for (var radio of radios)
-    {
-        if (radio.checked) {
-            Sexuality = radio.value;
-        }
-    }
-    var radios = document.getElementsByName('Neurodiversity');
-    for (var radio of radios)
-    {
-        if (radio.checked) {
-            Neurodiversity = radio.value;
-        }
-    }
-    var radios = document.getElementsByName('MentalHealth');
-    for (var radio of radios)
-    {
-        if (radio.checked) {
-            MentalHealth = radio.value;
-        }
-    }
-    var radios = document.getElementsByName('Body');
-    for (var radio of radios)
-    {
-        if (radio.checked) {
-            Body = radio.value;
-        }
-    }
-    var radios = document.getElementsByName('Housing');
-    for (var radio of radios)
-    {
-        if (radio.checked) {
-            Housing = radio.value;
-        }
-    }
-    var radios = document.getElementsByName('Wealth');
-    for (var radio of radios)
-    {
-        if (radio.checked) {
-            Wealth = radio.value;
-        }
-    }
-    var radios = document.getElementsByName('Language');
-    for (var radio of radios)
-    {
-        if (radio.checked) {
-            Language = radio.value;
-        }
-    }
-    var radios = document.getElementsByName('Gender');
-    for (var radio of radios)
-    {
-        if (radio.checked) {
-            Gender = radio.value;
-        }
-    }
-    var radios = document.getElementsByName('Citizenship');
-    for (var radio of radios)
-    {
-        if (radio.checked) {
-            Citizenship = radio.value;
-        }
-    }
-    chart.data.datasets[0].data = [SkinColour, Education, Ability, Sexuality, Neurodiversity, MentalHealth, Body, Housing, Wealth, Language, Gender, Citizenship];
-  	chart.update();
+  // We grab the labels from Labels again
+  labels = Object.keys(Labels)
+  // We iterate through each label, passing it into our function
+  labels.forEach(label => {
+    // We query the document for any input, with the name matching our label,
+    // that is checked. Then we grab the value.
+    // The backticks with the ${var} is called string interpolation - putting
+    // one string inside another.
+    value = document.querySelector(`input[name="${label}"]:checked`).value;
+
+    // Now we have the value we can reset that value in our Labels object.
+    // What we are basically saying is that for this key, in this object,
+    // set the value to the variable.
+    Labels[label] = value
+  });
+  // Again, we just grab all the values, which will have been updated by line
+  // 87
+  chart.data.datasets[0].data = Object.values(Labels);
+  chart.update();
 }
 
 /*
 
-//Attempting to get it to work on all the radios as a loop 
+//Attempting to get it to work on all the radios as a loop
 function updateForm() {
     var radios = document.getElementsByTagName('input');
     for (i = 0; i < radios.length; i++)
